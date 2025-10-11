1632f15139a15c418a3c71cccfc0f3dfd7321474
clare <202310411201@cdu.edu.cn>
2025-10-11 13:26:28 +0800

    初始化Spring Boot项目

---
+++ Forum/src/test/java/com/gxa/cddx/www/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxa.cddx.www.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
